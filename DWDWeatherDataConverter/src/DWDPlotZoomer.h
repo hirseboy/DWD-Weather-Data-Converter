a222d2961391663351153c5a7853b9274faa58a8
Stephan Hirth <stephan.hirth@tu-dresden.de>
2023-07-27 15:35:22 +0200

    Added plot zoomer

--- DWDWeatherDataConverter/src/DWDPlotZoomer.h
+++ DWDWeatherDataConverter/src/DWDPlotZoomer.h
@@ -1,31 +1,19 @@
-#ifndef DWDDateTimeScaleDrawH
-#define DWDDateTimeScaleDrawH
+#ifndef DWDPLOTZOOMER_H
+#define DWDPLOTZOOMER_H
 
-#include <qwt_plot_zoomer.h>
-#include <qwt_plot_canvas.h>
-#include <qwt_text.h>
+#include "qwt_plot_zoomer.h"
+#include "qwt_plot.h"
 
-#include <QCoreApplication>
-#include <QDateTime>
+class DWDPlotZoomer : public QwtPlotZoomer {
 
-class QPainter;
-class QPalette;
-
-class DWDPlotZoomer: public QwtPlotZoomer
-{
 public:
-	DWDPlotZoomer(QWidget *canvas):
-		QwtPlotZoomer(canvas)
+	DWDPlotZoomer(QWidget *widget) :
+		QwtPlotZoomer(QwtPlot::xBottom, QwtPlot::yLeft, widget, true)
 	{}
 
-protected:
-	virtual QwtText trackerTextF(const QPointF &pos) const
-	{
-		QDateTime dt = QDateTime::fromMSecsSinceEpoch(static_cast<qint64>(pos.x()));
-		return QString("%1, %2")
-				.arg(dt.toString("yyyy-MM-dd"))
-				.arg(pos.y());
+	QwtText trackerText(const QPoint &/*pos*/) const {
+		return QwtText("");
 	}
-};
 
-#endif // QwtDateTimeScaleDrawH
+};
+#endif // DWDPLOTZOOMER_H
