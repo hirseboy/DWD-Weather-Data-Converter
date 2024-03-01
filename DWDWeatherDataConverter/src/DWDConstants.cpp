b9be24f2d184d4a4ea51a30ca22384f4959ab9b7
Stephan Hirth <stephan.hirth@tu-dresden.de>
2024-03-01 16:58:22 +0100

    Increased file version

--- DWDWeatherDataConverter/src/DWDConstants.cpp
+++ DWDWeatherDataConverter/src/DWDConstants.cpp
@@ -6,7 +6,6 @@ const char * const VERSION = "1.1";
 const char * const LONG_VERSION = "1.1.2";
 
 const int WRONG_NUMBER = -9999;
-
 const char * const ORG_NAME = "IBK";
 
 const unsigned int CACHE_PERIOD = 7; // 7 DAYS
